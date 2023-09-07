3ec4b3201a651cf83b07921138aa1ddbc15404c1
AngelSushi <angelsushigame@gmail.com>
2023-09-07 19:08:06 +0200

    Clear Score

--- GodFather23URP/Assets/GameManager.cs
+++ GodFather23URP/Assets/GameManager.cs
@@ -1,14 +1,38 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
 
+    [Serializable]
+    public class ComboMultiplier
+    {
+        [SerializeField] private int insect;
+        [SerializeField] private float comboMultiplier;
+
+        public int Insect
+        {
+            get => insect;
+            set => insect = value;
+        }
+
+        public float Combo
+        {
+            get => comboMultiplier;
+            set => comboMultiplier = value;
+        }
+    }
+    
+    
     public static GameManager _instance;
     public List<GameObject> _listMonster;
 
     private bool _isInBoss;
+
+    [SerializeField] private int pointEat;
 
     public bool IsInBoss
     {
@@ -22,6 +46,8 @@ public class GameManager : MonoBehaviour
         _instance = this;
     }
 
+    [SerializeField] private List<ComboMultiplier> combos;
+
     //quand on fini un cercle complet
     public void WebIsFinish()
     {
@@ -32,6 +58,14 @@ public class GameManager : MonoBehaviour
             if (_combo)
                 _multiplier++;
         }
+
+
+        ComboMultiplier combo = combos.Where(combo => combo.Insect == _multiplier).FirstOrDefault();
+        float comboMultiplier = combo == null ? combos.Last().Combo : combo.Combo;
+
+// Probleme lorsqu'on ferme mal ca ajoute du score pour rien 
+        FindObjectOfType<ScoreManager>().Score =FindObjectOfType<ScoreManager>().Score + ((int)((pointEat * _multiplier) * comboMultiplier));
+
         Debug.Log("combo de : " + _multiplier + " insectes");
     }
 
