8edb8b78f5830f04e635fc1b59b142332671e8a2
hugo <hugo.monmousseau@hotmail.fr>
2023-09-07 11:30:38 +0200

    push avant de manger 😒😒

--- GodFather23URP/Assets/GameManager.cs
+++ GodFather23URP/Assets/GameManager.cs
@@ -25,7 +25,14 @@ public class GameManager : MonoBehaviour
     //quand on fini un cercle complet
     public void WebIsFinish()
     {
-
+        int _multiplier = 0;
+        for(int _loop = 0; _loop < _listMonster.Count; _loop++)
+        {
+            bool _combo = _listMonster[_loop].GetComponent<timerDead>().DoIMDead();
+            if (_combo)
+                _multiplier++;
+        }
+        Debug.Log("combo de : " + _multiplier + " insectes");
     }
 
 }
